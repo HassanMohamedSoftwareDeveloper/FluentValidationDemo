e3a4f1ea576bc896f99c62493db724e3e5e023ed
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-09-05 11:34:52 +0200

    Add project files.

---
+++ FluentValidationDemo/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using FluentValidationDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidationDemo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    private static readonly List<Customer> _customers = new();
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_customers);
+    }
+    [HttpPost("create-customer")]
+    public IActionResult Create(Customer customer)
+    {
+        if (!ModelState.IsValid)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, ModelState);
+        }
+        _customers.Add(customer);
+        return StatusCode(StatusCodes.Status200OK, "Model is valid!");
+    }
+    [HttpPut("update-customer/{id}")]
+    public IActionResult Update(int id, Customer custome, [FromServices] IValidator<Customer> customerValidator)
+    {
+        var validatorResult = customerValidator.Validate(custome);
+        if (!validatorResult.IsValid)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, validatorResult.Errors);
+        }
+        var customer = _customers.FirstOrDefault(x => x.Id == id);
+
+        if (customer is null) return NotFound();
+
+        customer.FirstName = custome.FirstName;
+        customer.LastName = custome.LastName;
+        customer.Address = custome.Address;
+        customer.Age = custome.Age;
+        customer.Phone = custome.Phone;
+
+
+        return StatusCode(StatusCodes.Status200OK, "Model is valid for update!");
+    }
+}
+
---
+++ FluentValidationDemo/Program.cs
@@ -0,0 +1,36 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+// builder.Services.AddFluentValidationClientsideAdapters();
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
